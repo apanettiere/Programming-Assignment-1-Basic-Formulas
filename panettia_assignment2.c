c16fcda129df447265e7de1f8760946e052dda4f
Andrew Panettiere <apanettiere.00@gmail.com>
2025-02-01 23:42:45 -0500

    Update panettia_assignment2.c

--- panettia_assignment2.c
+++ panettia_assignment2.c
@@ -3,91 +3,109 @@
 #include <string.h>
 
 typedef struct movie {
-    char title[256];
+    char *title;
     int year;
-    char languages[5][21];
+    char *languages;
     double rating;
-    struct movie* next;
-} movie;
+    struct movie *next;
+} Movie;
 
-movie* create_movie(char* title, int year, char languages[][21], double rating) {
-    movie* new_movie = (movie*)malloc(sizeof(movie));
-    if (new_movie == NULL) {
-        fprintf(stderr, "Failed to allocate memory for new movie.\n");
-        return NULL;
-    }
-    strcpy(new_movie->title, title);
-    new_movie->year = year;
-    for (int i = 0; i < 5; i++) {
-        strcpy(new_movie->languages[i], languages[i]);
-    }
-    new_movie->rating = rating;
-    new_movie->next = NULL;
-    return new_movie;
-}
+Movie *createMovie(char *line);
+Movie *processFile(char *filename);
+void showMoviesByYear(Movie *head);
+void showHighestRatedMovies(Movie *head);
+void showMoviesByLanguage(Movie *head);
+void freeMovies(Movie *head);
 
-void insert_movie(movie** head, movie* new_movie) {
-    if (*head == NULL) {
-        *head = new_movie;
-    } else {
-        movie* temp = *head;
-        while (temp->next != NULL) {
-            temp = temp->next;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./movies <filename>\n");
+        return EXIT_FAILURE;
+    }
+
+    Movie *movies = processFile(argv[1]);
+    int choice;
+
+    do {
+        printf("\n1. Show movies released in the specified year\n");
+        printf("2. Show highest rated movie for each year\n");
+        printf("3. Show the title and year of release of all movies in a specific language\n");
+        printf("4. Exit from the program\n\n");
+        printf("Enter a choice from 1 to 4: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                showMoviesByYear(movies);
+                break;
+            case 2:
+                showHighestRatedMovies(movies);
+                break;
+            case 3:
+                showMoviesByLanguage(movies);
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                break;
+            default:
+                printf("You entered an incorrect choice. Try again.\n");
         }
-        temp->next = new_movie;
-    }
+    } while (choice != 4);
+
+    freeMovies(movies);
+    return EXIT_SUCCESS;
 }
 
-movie* parse_line(char* line) {
-    char* token;
-    char title[256];
-    int year;
-    char languages[5][21] = {""};
-    double rating;
-    int lang_index = 0;
+Movie *createMovie(char *line) {
+    Movie *m = malloc(sizeof(Movie));
+    char *saveptr;
 
-    token = strtok(line, ",");
-    strcpy(title, token);
+    m->title = strdup(strtok_r(line, ",", &saveptr));
+    m->year = atoi(strtok_r(NULL, ",", &saveptr));
+    m->languages = strdup(strtok_r(NULL, ",", &saveptr));
+    m->rating = atof(strtok_r(NULL, "\n", &saveptr));
+    m->next = NULL;
 
-    token = strtok(NULL, ",");
-    year = atoi(token);
-
-    token = strtok(NULL, ",");
-    token = strtok(token + 1, ";]"); // Adjusted to skip '['
-    while (token && lang_index < 5) {
-        strcpy(languages[lang_index++], token);
-        token = strtok(NULL, ";]");
-    }
-
-    token = strtok(NULL, ",");
-    rating = strtod(token, NULL);
-
-    return create_movie(title, year, languages, rating);
+    return m;
 }
 
-movie* load_movies(char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Failed to open file %s\n", filename);
+Movie *processFile(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Could not open file %s\n", filename);
         return NULL;
     }
-    movie* head = NULL;
-    char buffer[1024];
-    int count = 0;
 
-    fgets(buffer, 1024, file); // Skip the header line
-    while (fgets(buffer, 1024, file)) {
-        buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
-        movie* new_movie = parse_line(buffer);
-        insert_movie(&head, new_movie);
-        count++;
+    char buffer[1024];
+    fgets(buffer, sizeof(buffer), file);  // Skip header
+
+    Movie *head = NULL, *tail = NULL;
+    while (fgets(buffer, sizeof(buffer), file)) {
+        Movie *newMovie = createMovie(buffer);
+        if (!tail) {
+            head = tail = newMovie;
+        } else {
+            tail->next = newMovie;
+            tail = newMovie;
+        }
     }
     fclose(file);
+
+    int count = 0;
+    Movie *temp = head;
+    while (temp) {
+        count++;
+        temp = temp->next;
+    }
     printf("Processed file %s and parsed data for %d movies\n", filename, count);
     return head;
 }
 
-void show_movies_by_year(movie* head, int year) {
+void showMoviesByYear(Movie *head) {
+    int year;
+    printf("Enter the year for which you want to see movies: ");
+    scanf("%d", &year);
+
     int found = 0;
     while (head) {
         if (head->year == year) {
@@ -101,38 +119,36 @@ void show_movies_by_year(movie* head, int year) {
     }
 }
 
-void show_highest_rated_movies(movie* head) {
-    struct {
-        int year;
-        double rating;
-        char title[256];
-    } best_movies[122] = {0}; // Assuming range from 1900 to 2021
+void showHighestRatedMovies(Movie *head) {
+    double highestRatings[122] = {0};  // Array for years 1900-2021
+    Movie *highestMovies[122] = {NULL};
 
     while (head) {
         int index = head->year - 1900;
-        if (head->rating > best_movies[index].rating) {
-            best_movies[index].rating = head->rating;
-            strcpy(best_movies[index].title, head->title);
+        if (head->rating > highestRatings[index]) {
+            highestRatings[index] = head->rating;
+            highestMovies[index] = head;
         }
         head = head->next;
     }
 
     for (int i = 0; i < 122; i++) {
-        if (best_movies[i].rating != 0) {
-            printf("%d %.1f %s\n", 1900 + i, best_movies[i].rating, best_movies[i].title);
+        if (highestMovies[i]) {
+            printf("%d %.1f %s\n", highestMovies[i]->year, highestMovies[i]->rating, highestMovies[i]->title);
         }
     }
 }
 
-void show_movies_by_language(movie* head, char* language) {
+void showMoviesByLanguage(Movie *head) {
+    char language[21];
+    printf("Enter the language for which you want to see movies: ");
+    scanf("%20s", language);  // Avoid buffer overflow
+
     int found = 0;
     while (head) {
-        for (int i = 0; i < 5; i++) {
-            if (strcmp(head->languages[i], language) == 0) {
-                printf("%d %s\n", head->year, head->title);
-                found = 1;
-                break;
-            }
+        if (strstr(head->languages, language)) {  // Simple substring match
+            printf("%d %s\n", head->year, head->title);
+            found = 1;
         }
         head = head->next;
     }
@@ -141,58 +157,14 @@ void show_movies_by_language(movie* head, char* language) {
     }
 }
 
-void free_movies(movie* head) {
+void freeMovies(Movie *head) {
     while (head) {
-        movie* temp = head;
+        Movie *temp = head;
         head = head->next;
+        free(temp->title);
+        free(temp->languages);
         free(temp);
     }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    movie* head = load_movies(argv[1]);
-    if (head == NULL) {
-        return EXIT_FAILURE;
-    }
-
-    int choice;
-    do {
-        printf("\n1. Show movies released in the specified year\n");
-        printf("2. Show highest rated movie for each year\n");
-        printf("3. Show the title and year of release of all movies in a specific language\n");
-        printf("4. Exit from the program\n");
-        printf("Enter a choice from 1 to 4: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter the year for which you want to see movies: ");
-                scanf("%d", &year);
-                show_movies_by_year(head, year);
-                break;
-            case 2:
-                show_highest_rated_movies(head);
-                break;
-            case 3:
-                printf("Enter the language for which you want to see movies: ");
-                scanf("%20s", language); // Limit input to 20 characters
-                show_movies_by_language(head, language);
-                break;
-            case 4:
-                printf("Exiting program.\n");
-                break;
-            default:
-                printf("You entered an incorrect choice. Try again.\n");
-                break;
-        }
-    } while (choice != 4);
-
-    free_movies(head);
-    return EXIT_SUCCESS;
-}
 
